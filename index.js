28a26fefdfae4d1c2a567406a48c76415b64e871
Ryan Werner <rmwerner@umich.edu>
2020-05-11 15:37:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 function happyHolidays() {return "Happy holidays!"}
 function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
+function happyCustomHolidaysTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
